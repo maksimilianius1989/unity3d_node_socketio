6253f1d4de52ba2b0e1b1f40b2584b0978778b6f
Vimax <it.vimax@gmail.com>
2019-04-01 17:56:06 +0300

    less 5

--- Assets/Scripts/Buillboard.cs
+++ Assets/Scripts/Buillboard.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Buildboard : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		transform.LookAt(Camera.main.transform);
 	}
 }
